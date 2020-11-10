24579c825316fda3a8f2b33b0f8b283f3d71aba4
lamaj-com <lamaj-com@users.noreply.github.com>
2020-11-10 12:31:30 +0100

    Using only alphanumerics and underscore for shortcode generation

--- src/main/java/com/example/urlshortener/service/ShortcodeGenerator.java
+++ src/main/java/com/example/urlshortener/service/ShortcodeGenerator.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ShortcodeGenerator {
 
-	private final static char allowedChars_64[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-"
-			.toCharArray();
-	private final static char allowedChars_62[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	private final static char allowedChars_63[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
 			.toCharArray();
 
 	/**
@@ -24,8 +22,8 @@ public class ShortcodeGenerator {
 	public String generateRandomShortcode(int length) {
 		StringBuilder shortcode = new StringBuilder(length);
 		for (int i = 0; i < length; i++) {
-			int randomIndex = ThreadLocalRandom.current().nextInt(0, allowedChars_64.length);
-			shortcode.append(allowedChars_64[randomIndex]);
+			int randomIndex = ThreadLocalRandom.current().nextInt(0, allowedChars_63.length);
+			shortcode.append(allowedChars_63[randomIndex]);
 		}
 		return shortcode.toString();
 	}
@@ -42,8 +40,8 @@ public class ShortcodeGenerator {
 		StringBuilder shortcode = new StringBuilder();
 		// Convert given integer id to a base 62 number
 		while (id > 0) {
-			shortcode.append(allowedChars_62[(int) (id % allowedChars_62.length)]);
-			id = id / allowedChars_62.length;
+			shortcode.append(allowedChars_63[(int) (id % allowedChars_63.length)]);
+			id = id / allowedChars_63.length;
 		}
 		return shortcode.reverse().toString();
 	}
@@ -59,11 +57,13 @@ public class ShortcodeGenerator {
 		long id = 0;
 		for (int i = 0; i < shortcode.length(); i++) {
 			if ('a' <= shortcode.charAt(i) && shortcode.charAt(i) <= 'z')
-				id = id * allowedChars_62.length + shortcode.charAt(i) - 'a';
+				id = id * allowedChars_63.length + shortcode.charAt(i) - 'a';
 			if ('A' <= shortcode.charAt(i) && shortcode.charAt(i) <= 'Z')
-				id = id * allowedChars_62.length + shortcode.charAt(i) - 'A' + 26;
+				id = id * allowedChars_63.length + shortcode.charAt(i) - 'A' + 26;
 			if ('0' <= shortcode.charAt(i) && shortcode.charAt(i) <= '9')
-				id = id * allowedChars_62.length + shortcode.charAt(i) - '0' + 52;
+				id = id * allowedChars_63.length + shortcode.charAt(i) - '0' + 52;
+			if ('_' == shortcode.charAt(i))
+				id = id * allowedChars_63.length + shortcode.charAt(i) - '_' + 62;
 		}
 		return id;
 	}
